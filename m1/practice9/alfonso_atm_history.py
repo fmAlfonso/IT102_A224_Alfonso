78d809465d84cbaaa7c6c0b3a4252da02ed634b0
FMalfonso <163765926+fmAlfonso@users.noreply.github.com>
2026-09-25 08:38:40 +0000

    Adding history codes

---
+++ m1/practice9/alfonso_atm_history.py
@@ -0,0 +1,20 @@
+def view_history():
+    try:
+        with open("transaction.txt", "r") as file:
+            lines = file.read()
+            return lines
+    except FileNotFoundError:
+        return []
+
+
+"""
+######### Learning Signature ######### 
+Programmed by: Favio Maximo Alfonso
+Date Submitted: September 25, 2026
+
+Program Description: This module handles viewing transaction history for an ATM account.
+Reflection: I learned how to do basic file handling in Python.
+
+AI Usage
+[ ] No AI Assistance – Completed independently without AI.
+"""
